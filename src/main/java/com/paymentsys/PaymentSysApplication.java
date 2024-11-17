2f160731ccf705124b75df543dd4ea38a34f93e6
myeonginjin <113578420+myeonginjin@users.noreply.github.com>
2024-11-17 22:49:13 +0900

    first commit

---
+++ src/main/java/com/paymentsys/PaymentSysApplication.java
@@ -0,0 +1,13 @@
+package com.paymentsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentSysApplication.class, args);
+    }
+
+}
